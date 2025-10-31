111e566fffd536e1c5e13fb6e16dce787808d62b
Tim <tim.yi@chemolight.co.uk>
2025-10-31 21:41:11 +0000

    Unit test case updated

---
+++ tests/test_operation.py
@@ -0,0 +1,11 @@
+from src.math_operation import add, subtract
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+
+def test_subtract():
+    assert subtract(5, 3) == 2
+    assert subtract(0, 0) == 0
+    assert subtract(-1, -1) == 0
+    assert subtract(10, 5) == 5
